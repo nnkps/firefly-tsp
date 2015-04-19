45eabee14ee6b6932df28517218e22964319a54b
mciolek <maciek.ciolek@gmail.com>
2015-04-20 00:34:25 +0200

    Improvements

--- gui-app/build/tmp/templates.js
+++ gui-app/build/tmp/templates.js
@@ -3,6 +3,20 @@ try { module = angular.module("templates"); }
 catch(err) { module = angular.module("templates", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
+  $templateCache.put("modules/main/directive/graph/graph.html",
+    "<div id=\"graph-workspace\">\n" +
+    "\n" +
+    "\n" +
+    "</div>\n" +
+    "");
+}]);
+})();
+
+(function(module) {
+try { module = angular.module("templates"); }
+catch(err) { module = angular.module("templates", []); }
+module.run(["$templateCache", function($templateCache) {
+  "use strict";
   $templateCache.put("modules/main/partial/index/index.html",
     "<div class=\"row\">\n" +
     "    <div class=\"col-lg-12\">\n" +
@@ -15,7 +29,7 @@ module.run(["$templateCache", function($templateCache) {
     "                        <label for=\"fCount\" class=\"col-sm-2 control-label\">Firefly count:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fCount\"\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fCount\"\n" +
     "                                   ng-model=\"vm.parameters.number_of_individuals\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
@@ -23,7 +37,7 @@ module.run(["$templateCache", function($templateCache) {
     "                        <label for=\"fCount2\" class=\"col-sm-2 control-label\">Number of cities:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fCount2\"\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fCount2\"\n" +
     "                                   ng-model=\"vm.parameters.number_of_cities\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
@@ -31,21 +45,21 @@ module.run(["$templateCache", function($templateCache) {
     "                        <label for=\"fFactorAlpha\" class=\"col-sm-2 control-label\">Factor &alpha;:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fFactorAlpha\" ng-model=\"vm.parameters.alpha\">\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fFactorAlpha\" ng-model=\"vm.parameters.alpha\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
     "                    <div class=\"form-group\">\n" +
     "                        <label for=\"fFactorBeta\" class=\"col-sm-2 control-label\">Factor &beta;:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fFactorBeta\" ng-model=\"vm.parameters.beta\">\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fFactorBeta\" ng-model=\"vm.parameters.beta\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
     "                    <div class=\"form-group\">\n" +
     "                        <label for=\"fFactorGamma\" class=\"col-sm-2 control-label\">Factor &gamma;:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fFactorGamma\" ng-model=\"vm.parameters.gamma\">\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fFactorGamma\" ng-model=\"vm.parameters.gamma\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
     "\n" +
@@ -53,7 +67,7 @@ module.run(["$templateCache", function($templateCache) {
     "                        <label for=\"fIterationsCount\" class=\"col-sm-2 control-label\">Iterations count:</label>\n" +
     "\n" +
     "                        <div class=\"col-sm-10\">\n" +
-    "                            <input type=\"text\" class=\"form-control\" id=\"fIterationsCount\"\n" +
+    "                            <input type=\"number\" class=\"form-control\" id=\"fIterationsCount\"\n" +
     "                                   ng-model=\"vm.parameters.iterations\">\n" +
     "                        </div>\n" +
     "                    </div>\n" +
@@ -72,20 +86,6 @@ module.run(["$templateCache", function($templateCache) {
     "            </div>\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "\n" +
-    "</div>\n" +
-    "");
-}]);
-})();
-
-(function(module) {
-try { module = angular.module("templates"); }
-catch(err) { module = angular.module("templates", []); }
-module.run(["$templateCache", function($templateCache) {
-  "use strict";
-  $templateCache.put("modules/main/directive/graph/graph.html",
-    "<div id=\"graph-workspace\">\n" +
-    "\n" +
     "\n" +
     "</div>\n" +
     "");
